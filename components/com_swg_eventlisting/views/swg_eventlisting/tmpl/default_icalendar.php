e8fd40f4dfade2e8e497c75c725830d9b85554a2
Peter Copeland <peter@tong.(none)>
2012-06-11 02:10:14 +0100

    Initial work on iCalendar events page

---
+++ components/com_swg_eventlisting/views/swg_eventlisting/tmpl/default_icalendar.php
@@ -0,0 +1,19 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+?>
+BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:TODO
+<?php foreach ($this->events as $event) {?>
+BEGIN:VEVENT
+UID:<?php echo $event->getEventType();?>No<?php echo $event->id."\n";?>
+DTSTAMP:TODO
+ORGANIZER:CN=Sheffield Walking Group:MAILTO:sheffieldwalkinggroup@hotmail.com
+DTSTART:<?php echo strftime("%Y%m%dT%H%M%SZ", $event->startDate)."\n";?>
+DTEND:TODO
+SUMMARY:<?php echo $event->name."\n"; ?>
+<?php // TODO: iCalendar can handle cancelled events & updates. Look up full documentation. ?>
+END:VEVENT
+<?php } ?>
+END:VCALENDAR
---
+++ components/com_swg_eventlisting/views/swg_eventlisting/view.icalendar.php
@@ -0,0 +1,28 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+// import Joomla view library
+jimport('joomla.application.component.view');
+ 
+/**
+ * HTML View class for the SWG_EventListing Component
+ */
+class SWG_EventListingViewSWG_EventListing extends JView
+{
+	// Overwriting JView display method
+	function display($tpl = "icalendar") 
+	{
+		// Assign data to the view
+		$this->events = $this->get('Events');
+ 
+		// Check for errors.
+		if (count($errors = $this->get('Errors'))) 
+		{
+			JError::raiseError(500, implode('<br />', $errors));
+			return false;
+		}
+		// Display the view
+		parent::display($tpl);
+	}
+}
