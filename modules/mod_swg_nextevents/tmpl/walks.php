1df395237e7dd499062289647bf447c2074cab30
Peter Copeland <peter@tong.(none)>
2012-06-16 02:12:26 +0100

    Remove extra HTML for walk info - it's created by JS

--- modules/mod_swg_nextevents/tmpl/walks.php
+++ modules/mod_swg_nextevents/tmpl/walks.php
@@ -11,33 +11,3 @@
   </ul>
   <p><a href="<?php echo $listPage;?>" class="more">More</a></p>
 </div>
-<div class="event popup walk" id="walk-popup" style="display:none;">
-   <div class="eventheader">
-      <span class="date"></span>
-      <span class="rating"></span>
-      <h3></h3>
-    </div>
-    <div class="description">
-      <p></p>
-      <p class="icons"></p>
-    </div>
-    <div class="eventinfo">
-      <p class="start">
-        <span>Start:</span>
-          <a href="" title="Streetmap view of approximate location"></a>
-      </p>
-      <p class="end">
-        <span>End:</span>
-      </p>
-      <p class="transport">
-        <span>Transport:</span> TODO: Transport
-      </p>
-      <p class="leader">
-        <span>Walk Leader:</span> TODO: Leader
-      </p>
-      <p class="backmarker">
-        <span>Backmarker:</span> TODO: Backmarker
-      </p>
-    </div>
-    <div style="clear:right;">&nbsp;</div>
-  </div>
