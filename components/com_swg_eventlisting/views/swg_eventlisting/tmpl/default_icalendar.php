21c67975105c32ce7c9b9c380d972aae595daec4
Peter Copeland <peter@tong.(none)>
2012-06-18 01:47:18 +0100

    Trying adding leaders as organisers
    May need to just put leader info as text - iCalendar
    standard sends automatic emails to organiser when
    someone adds themselves as a participant

--- components/com_swg_eventlisting/views/swg_eventlisting/tmpl/default_icalendar.php
+++ components/com_swg_eventlisting/views/swg_eventlisting/tmpl/default_icalendar.php
@@ -6,40 +6,48 @@ BEGIN:VCALENDAR
 VERSION:2.0
 <?php foreach ($this->events as $event) {?>
 BEGIN:VEVENT
-UID:<?php echo $event->getEventType();?>No<?php echo $event->id."\n";?>
-CATEGORIES:<?php echo ucfirst($event->getEventType())."\n"; ?>
-ORGANIZER;CN=Sheffield Walking Group:MAILTO:sheffieldwalkinggroup@hotmail.com
-DTSTART:<?php echo strftime("%Y%m%dT%H%M%S", $event->startDate)."\n";?>
+UID:<?php echo $event->getEventType();?>No<?php echo $event->id."\r\n";?>
+CATEGORIES:<?php echo ucfirst($event->getEventType())."\r\n"; ?>
+ORGANIZER;<?php
+  // For a walk, the organiser is the leader. For a social or weekend, it's the organiser
+  if ($event instanceof WalkInstance) {
+    echo "CN=".$event->leader->displayName." (".$event->leader->telephone.") - don't call during :MAILTO:sheffieldwalkinggroup@hotmail.com";
+  }
+  else if ($event instanceof Social) {
+    echo "CN=Sheffield Walking Group:MAILTO:sheffieldwalkinggroup@hotmail.com";
+  }
+  else if ($event instanceof Weekend) {
+    echo "CN=Sheffield Walking Group:MAILTO:sheffieldwalkinggroup@hotmail.com";
+  }
+  else {
+    echo "CN=Sheffield Walking Group:MAILTO:sheffieldwalkinggroup@hotmail.com";
+  }
+  echo "\r\n";
+?>
+DTSTART:<?php echo strftime("%Y%m%dT%H%M%S", $event->startDate)."\r\n";?>
 SUMMARY:<?php
   echo $event->name;
   if ($event instanceof WalkInstance) {
     echo " (".$event->distanceGrade.$event->difficultyGrade." - ".$event->miles." miles)";
   }
-  echo "\n";
+  echo "\r\n";
 ?>
-DESCRIPTION:<?php echo $this->parseText($event->description)."\n";?>
-CONTACT:<?php 
-// TODO: Other contact info, e.g. no contact office hours
-if ($event instanceof WalkInstance) {
-  echo "Leader TODO"; // TODO: Backmarker todo too
-}
-else if ($event instanceof Social) {
-  echo $event->bookingsInfo; // TODO: This will probably need parsing
-}
-else if ($event instanceof Weekend) {
-  echo $event->contact;
-}
-echo "\n";
+DESCRIPTION:<?php echo $this->parseText($event->description)."\r\n";?>
+<?php 
+  // Contacts are specified in different formats for different events
+  if ($event instanceof WalkInstance) {
+    echo "ATTENDEE;ROLE=CHAIR;CN=".$event->leader->displayName.":MAILTO:sheffieldwalkinggroup@example.com\r\n";
+  } 
 echo "DTEND:"; 
   if ($event instanceof WalkInstance) {
-    echo strftime("%Y%m%dT%H%M%S", $event->estimateFinishTime())."\n";
+    echo strftime("%Y%m%dT%H%M%S", $event->estimateFinishTime())."\r\n";
   } else if ($event instanceof Weekend) {
     // End time should be midnight of the day after the last one for an all day event
     echo strftime("%Y%m%dT%H%M%S", $event->endDate+86400);
   } else if ($event instanceof Social) {
     echo strftime("%Y%m%dT%H%M%S", $event->startDate);
   }
-echo "\n";
+echo "\r\n";
 // TODO: iCalendar can handle cancelled events & updates. Look up full documentation. ?>
 END:VEVENT
 <?php } ?>
