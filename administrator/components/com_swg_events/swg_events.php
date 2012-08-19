54ade297c1f55951c9fbde48a69bba45d0f8763e
Peter Copeland <peter@tong.(none)>
2012-08-19 23:29:14 +0100

    Initial commit of admin area - can set permissions

--- administrator/components/com_swg_events/swg_events.php
+++ administrator/components/com_swg_events/swg_events.php
@@ -1,1 +1,21 @@
-Hello administrator
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+// Access check.
+if (!JFactory::getUser()->authorise('core.manage', 'com_helloworld'))
+{
+  return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
+}
+ 
+// import joomla controller library
+jimport('joomla.application.component.controller');
+ 
+// Get an instance of the controller prefixed by HelloWorld
+$controller = JController::getInstance('SWG_Events');
+ 
+// Perform the Request task
+$controller->execute(JRequest::getCmd('task'));
+ 
+// Redirect if set by the controller
+//$controller->redirect();
---
+++ administrator/components/com_swg_events/views/swg_events/tmpl/default.php
@@ -0,0 +1,14 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted Access');
+
+// load tooltip behavior
+JHtml::_('behavior.tooltip');
+?>
+<form action="<?php echo JRoute::_('index.php?option=com_swg_events'); ?>" method="post" name="adminForm">
+	<table class="adminlist">
+		<thead>HEAD</thead>
+		<tfoot>FOOT</tfoot>
+		<tbody>BODY</tbody>
+	</table>
+</form>
