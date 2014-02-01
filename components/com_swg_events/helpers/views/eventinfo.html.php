a08c31cdfc6ef7d72bacb25a9f599df53b69cb2e
Peter Copeland <dolphin@dphin.co.uk>
2014-02-01 11:36:44 +0000

    Share the view PHP for event info

---
+++ components/com_swg_events/helpers/views/eventinfo.html.php
@@ -0,0 +1,101 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+// import Joomla view library
+jimport('joomla.application.component.view');
+
+/**
+* Methods shared by all event info views
+*/
+abstract class SWG_EventsHelperEventInfo extends JView
+{
+	/**
+	 * Adds the map javascript to the document
+	 */
+	protected function mapJS(JDocument $document)
+	{
+		// Add map interface Javascript
+		JHtml::_('behavior.framework', true);
+		$document->addScript('/libraries/openlayers/OpenLayers.debug.js');
+		$document->addScript('/swg/js/maps.js');
+		$document->addScript('/swg/js/events.js');
+		$document->addScript('/components/com_swg_events/views/eventlisting/script/eventlisting.js');	
+	}
+	
+	/**
+	* True if the given date is NOT this year
+	* @param int $date
+	* @return boolean
+	*/
+	public function notThisYear($date)
+	{
+		return (date("Y", $date) != date("Y"));
+	}
+	
+	/**
+	* True if two dates have DIFFERENT months. Ignores year
+	* @param int $date1
+	* @param int $date2
+	*/
+	public function notSameMonth($date1, $date2)
+	{
+		return (date("m", $date1) != date("m",$date2));
+	}
+	
+	/**
+	* True if time is not midnight. 
+	* We're making an assumption here that no event will start at midnight,
+	* but if that's wrong all that happens is the start time doesn't appear in the event info
+	* @param int $timestamp
+	*/
+	public function isTimeSet($timestamp)
+	{
+		return (date("His", $timestamp) != 0);
+	}
+	
+	/**
+	 * True if the event is in the past.
+	 * In practice, we check if the event's end date (if any) or start date is on a day before today.
+	 * @param Event $evt
+	 * @return bool
+	 */
+	public function eventInPast(Event $event)
+	{
+		if (isset($evt->endDate))
+		{
+			$evtDate = $event->endDate;
+		}
+		else
+		{
+			$evtDate = $event->start;
+		}
+		
+		return (unixtojd($evtDate) < unixtojd(time()));
+	}
+	
+	public function showEditLinks($event)
+	{
+		return (
+			JRequest::getBool("showEditOptions") && 
+			SWG_EventsController::canEdit($event) && 
+			(
+				($event instanceof WalkInstance && $this->addEditWalkURL()) ||
+				($event instanceof Social && $this->addEditSocialURL()) ||
+				($event instanceof Weekend && $this->addEditWeekendURL())
+			)
+		);
+	}
+	
+	public function editURL($event)
+	{
+		if ($event instanceof WalkInstance)
+			return $this->addEditWalkURL()."?walkinstanceid=".$event->id;
+		else if ($event instanceof Social)
+			return $this->addEditSocialURL()."?socialid=".$event->id;
+		else if ($event instanceof Weekend)
+			return $this->addEditWeekendURL()."?weekendid=".$event->id;
+		else
+			return "";
+	}
+}
--- components/com_swg_events/views/eventlisting/view.html.php
+++ components/com_swg_events/views/eventlisting/view.html.php
@@ -5,10 +5,12 @@ defined('_JEXEC') or die('Restricted access');
 // import Joomla view library
 jimport('joomla.application.component.view');
 
+include_once(JPATH_SITE."/components/com_swg_events/helpers/views/eventinfo.html.php");
+
 /**
 * HTML Event listing class for the SWG Events component
 */
-class SWG_EventsViewEventListing extends JView
+class SWG_EventsViewEventListing extends SWG_EventsHelperEventInfo
 {
 	// Overwriting JView display method
 	function display($tpl = null) 
@@ -24,13 +26,8 @@ class SWG_EventsViewEventListing extends JView
 			return false;
 		}
 		
-		// Add map interface Javascript
 		$document = JFactory::getDocument();
-		JHtml::_('behavior.framework', true);
-		$document->addScript('/libraries/openlayers/OpenLayers.debug.js');
-		$document->addScript('/swg/js/maps.js');
-		$document->addScript('/swg/js/events.js');
-		$document->addScript('/components/com_swg_events/views/eventlisting/script/eventlisting.js');
+		$this->mapJS($document);
 		$totalEvents = $this->get('NumEvents');
 		$apiParams = json_encode($this->get('ApiParams'));
 		$document->addScriptDeclaration(<<<MAP
@@ -41,62 +38,11 @@ window.addEvent('domready', function()
 	totalEvents = {$totalEvents};
 	apiParams = {$apiParams};
 });
-			
 MAP
-);		
+);	
+
 		// Display the view
 		parent::display($tpl);
-	}
-	
-	/**
-	* True if the given date is NOT this year
-	* @param unknown_type $date
-	* @return boolean
-	*/
-	function notThisYear($date)
-	{
-		return (date("Y", $date) != date("Y"));
-	}
-	
-	/**
-	* True if two dates have DIFFERENT months. Ignores year
-	* @param unknown_type $date1
-	* @param unknown_type $date2
-	*/
-	function notSameMonth($date1, $date2)
-	{
-		return (date("m", $date1) != date("m",$date2));
-	}
-	
-	/**
-	* True if time is not midnight. 
-	* We're making an assumption here that no event will start at midnight,
-	* but if that's wrong all that happens is the start time doesn't appear in the event info
-	* @param  $timestamp
-	*/
-	function isTimeSet($timestamp)
-	{
-		return (date("His", $timestamp) != 0);
-	}
-	
-	/**
-	 * True if the event is in the past.
-	 * In practice, we check if the event's end date (if any) or start date is on a day before today.
-	 * @param Event $evt
-	 * @return bool
-	 */
-	function eventInPast(Event $event)
-	{
-		if (isset($evt->endDate))
-		{
-			$evtDate = $event->endDate;
-		}
-		else
-		{
-			$evtDate = $event->start;
-		}
-		
-		return (unixtojd($evtDate) < unixtojd(time()));
 	}
 	
 	function showAnyAddLinks()
@@ -149,28 +95,4 @@ MAP
 		return $link;
 	}
 	
-	function showEditLinks($event)
-	{
-		return (
-			JRequest::getBool("showEditOptions") && 
-			SWG_EventsController::canEdit($event) && 
-			(
-				($event instanceof WalkInstance && $this->addEditWalkURL()) ||
-				($event instanceof Social && $this->addEditSocialURL()) ||
-				($event instanceof Weekend && $this->addEditWeekendURL())
-			)
-		);
-	}
-	
-	function editURL($event)
-	{
-		if ($event instanceof WalkInstance)
-			return $this->addEditWalkURL()."?walkinstanceid=".$event->id;
-		else if ($event instanceof Social)
-			return $this->addEditSocialURL()."?socialid=".$event->id;
-		else if ($event instanceof Weekend)
-			return $this->addEditWeekendURL()."?weekendid=".$event->id;
-		else
-			return "";
-	}
 }
