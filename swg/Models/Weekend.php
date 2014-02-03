f7cb7eecc44767c8fab122ab63154ccaf942150e
Peter Copeland <dolphin@dphin.co.uk>
2014-02-03 23:31:49 +0000

    Manually calculate payment due date
    Looks like strtotime can't parse "last day of -1 month" until PHP 5.3

--- swg/Models/Weekend.php
+++ swg/Models/Weekend.php
@@ -172,7 +172,8 @@ public function __get($name)
 					$this->$name = $value;
 					// Calculate the payment due date.
 					// Payment is due by the end of the month before the weekend.
-					$this->paymentDue = strtotime("last day of -1 month", $value);
+					// Looks like "last day of -1 month" is only supported by PHP 5.3, so let's do this the old-fashioned way
+					$this->paymentDue = strtotime(strftime("%Y-%m-01", $value)) - 86400;
 				}
 				else
 				{
