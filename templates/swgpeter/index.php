fec939a47596cab2775c4268d6e0fb6c4f78f91d
Peter Copeland <dolphin@dphin.co.uk>
2014-04-09 23:12:03 +0100

    Remove old fixed viewport width directive

--- templates/swgpeter/index.php
+++ templates/swgpeter/index.php
@@ -19,7 +19,6 @@
 			@import url("<?php echo $this->baseurl;?>/templates/<?php echo $this->template;?>/css/template.css");
 			@import url("<?php echo $this->baseurl;?>/templates/<?php echo $this->template;?>/css/mobile.css") screen and (max-device-width:600px);
         </style>
-        <meta name="viewport" content="width=1016">
         <!--[if lt IE 9]>
             <script>
                 document.createElement('header');
