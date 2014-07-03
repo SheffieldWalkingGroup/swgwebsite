1d50c67865504bb237bee5a5ff1c3b8ffd60b32b
Peter Copeland <dolphin@dphin.co.uk>
2014-07-03 20:16:45 +0100

    Move makeScrollbar function out of private scope into template.js
    Fixes scrolling on homepage popups

--- templates/swgpeter/script/template.js
+++ templates/swgpeter/script/template.js
@@ -89,6 +89,40 @@ function Popup(title, body, type)
 	mask.show();
 }
 
+/**
+ * makeScrollbar function by Bas Wenneker, http://www.solutoire.com/experiments/scrollbar/index.html
+ * @param content
+ * @param scrollbar
+ * @param handle
+ * @param horizontal
+ * @param ignoreMouse
+ */
+function makeScrollbar(content,scrollbar,handle,horizontal,ignoreMouse){
+	var steps = (horizontal?(content.getScrollSize().x - content.getSize().x):(content.getScrollSize().y - content.getSize().y))
+	var slider = new Slider(scrollbar, handle, {	
+		steps: steps,
+		mode: (horizontal?'horizontal':'vertical'),
+		initialSetp:0,
+		onChange: function(step){
+			// Scrolls the content element in x or y direction.
+			var x = (horizontal?step:0);
+			var y = (horizontal?0:step);
+			content.scrollTo(x,y);
+		}
+	}).set(0);
+	if( !(ignoreMouse) ){
+		// Scroll the content element when the mousewheel is used within the 
+		// content or the scrollbar element.
+		$$(content, scrollbar).addEvent('mousewheel', function(e){	
+			e.stop();
+			var step = slider.step - e.wheel * 30;	
+			slider.set(step);					
+		});
+	}
+	// Stops the handle dragging process when the mouse leaves the document body.
+	$(document.body).addEvent('mouseleave',function(){slider.drag.stop()});
+}
+
 var Mobile = new Class({
 	mobile: false,
 	
