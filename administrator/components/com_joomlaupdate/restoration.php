59ba786c2fdac3dc8be658fd85496bffbb8b8ef9
Sheffield 20s and 30s Walking Group <sheffieldwalkinggroup@hotmail.com>
2018-11-01 19:00:46 +0000

    Updates to the website since last commit

---
+++ administrator/components/com_joomlaupdate/restoration.php
@@ -0,0 +1,13 @@
+<?php
+defined('_AKEEBA_RESTORATION') or die('Restricted access');
+$restoration_setup = array(
+	'kickstart.security.password' => '7C7joQpCmhLOyO2XPw799sTSnQSMBNXE',
+	'kickstart.tuning.max_exec_time' => '5',
+	'kickstart.tuning.run_time_bias' => '75',
+	'kickstart.tuning.min_exec_time' => '0',
+	'kickstart.procengine' => 'direct',
+	'kickstart.setup.sourcefile' => '/home/swg/www.sheffieldwalkinggroup.org.uk_html/tmp/juzCal0T',
+	'kickstart.setup.destdir' => '/home/web/SERVERS/www.sheffieldwalkinggroup.org.uk/docs',
+	'kickstart.setup.restoreperms' => '0',
+	'kickstart.setup.filetype' => 'zip',
+	'kickstart.setup.dryrun' => '0');
